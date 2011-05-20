5e4a49c7d99faac6d7c75cdba9df00be1ab58e1d
Ryan Petrello <lists@ryanpetrello.com>
2011-05-19 22:07:57 -0400

    Unit tests for pythonzombie.server.

--- pythonzombie/tests/test_server.py
+++ pythonzombie/tests/test_server.py
@@ -7,6 +7,10 @@ import fudge
 import subprocess
 import os
 
+class FakeNode(object):
+    def __json__(self):
+        return 'ENCODED'
+
 
 class FakePopen(object):
 
@@ -32,14 +36,14 @@ class TestServerSpawn(TestCase):
             subprocess,
             'Popen',
             (fudge.Fake('Popen').
-                    is_callable().
-                    with_args(
-                        args,
-                        stdin = subprocess.PIPE,
-                        stdout = subprocess.PIPE,
-                        stderr = subprocess.STDOUT
-                    ).
-                    returns(FakePopen()))
+                is_callable().
+                with_args(
+                    args,
+                    stdin = subprocess.PIPE,
+                    stdout = subprocess.PIPE,
+                    stderr = subprocess.STDOUT
+                ).
+                returns(FakePopen()))
             ):
             ZombieProxyServer()
 
@@ -63,6 +67,28 @@ class TestServerSpawn(TestCase):
             ):
             ZombieProxyServer(socket='/tmp/zombie-custom.sock')
 
+    @fudge.with_fakes
+    def test_stdout_redirect_exception(self):
+
+        args = ['env', 'node', ZombieProxyServer.__proxy_path__(), '/tmp/zombie.sock']
+        fake = FakePopen()
+        fake.stdout = None
+
+        with fudge.patched_context(
+            subprocess,
+            'Popen',
+            (fudge.Fake('Popen').
+                is_callable().
+                with_args(
+                    args,
+                    stdin = subprocess.PIPE,
+                    stdout = subprocess.PIPE,
+                    stderr = subprocess.STDOUT
+                ).
+                returns(fake))
+            ):
+            ZombieProxyServer()
+
 
 class TestServerCommunication(TestCase):
     
@@ -72,6 +98,7 @@ class TestServerCommunication(TestCase):
 
     def tearDown(self):
         super(TestServerCommunication, self).tearDown()
+        fudge.clear_expectations()
         self.server.kill()
 
     def test_server_running(self):
@@ -101,14 +128,64 @@ class TestServerCommunication(TestCase):
         }
         self.server.__encode__(foo) == dumps(foo)
 
-        class Fake(object):
-            def __json__(self):
-                return 'ENCODED'
-
-        self.server.__encode__(Fake()) == 'ENCODED'
+        self.server.__encode__(FakeNode()) == 'ENCODED'
 
     def test_decode(self):
         foo = dumps({
             'foo': 'bar'
         })
         self.server.__decode__(foo) == loads(foo)
+
+    def test_simple_send(self):
+        self.server.send(
+            "stream.end()"
+        )
+
+    def test_simple_json(self):
+        obj = {
+            'foo'   : 'bar',
+            'test'  : 500
+        }
+        assert self.server.json(obj) == obj
+
+    @fudge.with_fakes
+    def test_simple_wait(self):
+
+        js = """
+        browser.visit("http://example.com", function(err, browser){
+            if(err)
+                stream.end(JSON.stringify(err.stack));
+            else    
+                stream.end();
+        });
+        """
+
+        with fudge.patched_context(
+            ZombieProxyServer,
+            '__send__',
+            (fudge.Fake('__send__', expect_call=True).
+                with_args(js)
+            )):
+
+            self.server.wait('visit', 'http://example.com')
+
+    @fudge.with_fakes
+    def test_wait_without_arguments(self):
+
+        js = """
+        browser.wait(null, function(err, browser){
+            if(err)
+                stream.end(JSON.stringify(err.stack));
+            else    
+                stream.end();
+        });
+        """
+
+        with fudge.patched_context(
+            ZombieProxyServer,
+            '__send__',
+            (fudge.Fake('__send__', expect_call=True).
+                with_args(js)
+            )):
+
+            self.server.wait('wait')
