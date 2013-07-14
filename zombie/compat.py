eeb894612ec323fa723dd8aa4728f14cebdf723b
Javier Gonel <bolibic@gmail.com>
2013-07-14 15:37:12 +0300

    Remove compat.py from coverage

--- zombie/compat.py
+++ zombie/compat.py
@@ -4,10 +4,10 @@ import sys
 PY3 = sys.version_info[0] == 3
 PY26 = sys.version_info[0] == 2 and sys.version_info[1] == 6
 
-if PY3:  # pragma: nocover
+if PY3:
     from io import BytesIO as StringIO
     from urllib.parse import urlparse
-else:  # pragma: nocover
+else:
     from urlparse import urlparse  # noqa
     from cStringIO import StringIO  # noqa
 
